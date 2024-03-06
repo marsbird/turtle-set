5ac9f9c239f9d11abe9d6fc054653d10eaba5a20
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-06 12:42:58 -0500

    Prototype refresh dispatch

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { refresh } from '../slices/tileSlice.js';
 import Tile from './Tile.jsx';
 
 const Board = () => {
+  const values = useSelector((state) => state.tiles.values);
+  const dispatch = useDispatch();
+
   const tiles = [];
   for (let i = 0; i < 12; i++) {
-    tiles.push(<Tile key={i} />);
+    tiles.push(
+      <Tile
+        key={i}
+        number={values[i].number}
+        pattern={values[i].pattern}
+        color={values[i].color}
+        shape={values[i].shape}
+      />
+    );
   }
 
-  return <div>{tiles}</div>;
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={(e) => {
+          dispatch(refresh());
+        }}
+      >
+        New Game
+      </button>
+      <div>{tiles}</div>
+    </div>
+  );
 };
 
 export default Board;
--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Tile = () => {
-  return <div>hello</div>;
+const Tile = ({ number, pattern, color, shape }) => {
+  return (
+    <div>
+      num: {number}, pat: {pattern}, col: {color}, sha: {shape}
+    </div>
+  );
 };
 
 export default Tile;
