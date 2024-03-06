58327a020f922923ed5610e6eeb6020c4b7fdaa5
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-06 11:16:14 -0500

    Create components folder and move App component

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import App from './App.jsx';
+import App from './components/App.jsx';
 import store from './store.js';
 
 const root = createRoot(document.querySelector('#root'));
