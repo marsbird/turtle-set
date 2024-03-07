ed06660fdb0733a6c45f5c2dc9a64669fdbc8c28
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-07 00:36:16 -0500

    Install dependency: file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
         test: /\.s?css/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
