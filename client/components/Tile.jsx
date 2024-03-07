c8fde0629fc6dfbbfebabad0a7595b10056f935b
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-07 00:36:39 -0500

    Serve imgs based on state

--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -1,6 +1,15 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addUserSelection, removeUserSelection } from '../slices/tileSlice.js';
+import turtle from '../img/turtle.png';
+import turtleFill from '../img/turtle-fill.png';
+import turtleEmpty from '../img/turtle-empty.png';
+import starfish from '../img/starfish.png';
+import starfishFill from '../img/starfish-fill.png';
+import starfishEmpty from '../img/starfish-empty.png';
+import fish from '../img/fish.png';
+import fishFill from '../img/fish-fill.png';
+import fishEmpty from '../img/fish-empty.png';
 
 const Tile = ({ _id, number, pattern, color, shape }) => {
   const dispatch = useDispatch();
@@ -9,6 +18,54 @@ const Tile = ({ _id, number, pattern, color, shape }) => {
   let classes = 'tile';
   if (userSelection.includes(_id)) {
     classes += ' selected';
+  }
+
+  let icon;
+  switch (shape) {
+    case 0:
+      switch (pattern) {
+        case 0:
+          icon = turtle;
+          break;
+        case 1:
+          icon = turtleFill;
+          break;
+        case 2:
+          icon = turtleEmpty;
+          break;
+      }
+      break;
+    case 1:
+      switch (pattern) {
+        case 0:
+          icon = starfish;
+          break;
+        case 1:
+          icon = starfishFill;
+          break;
+        case 2:
+          icon = starfishEmpty;
+          break;
+      }
+      break;
+    case 2:
+      switch (pattern) {
+        case 0:
+          icon = fish;
+          break;
+        case 1:
+          icon = fishFill;
+          break;
+        case 2:
+          icon = fishEmpty;
+          break;
+      }
+      break;
+  }
+
+  const icons = [];
+  for (let i = 0; i <= number; i++) {
+    icons.push(icon);
   }
 
   return (
@@ -22,7 +79,11 @@ const Tile = ({ _id, number, pattern, color, shape }) => {
         }
       }}
     >
-      {_id} {number} {pattern} {color} {shape}
+      {icons.map((icon, i) => {
+        return (
+          <img key={i} src={icon} alt='' className={`icon-color-${color}`} />
+        );
+      })}
     </div>
   );
 };
