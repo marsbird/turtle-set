2dc48b40a623c31a1ae26a703870d0736c847a3d
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-10 11:58:10 -0400

    Create new component: Instructions

---
+++ client/components/Instructions.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import turtle from '../img/turtle.png';
+import turtleFill from '../img/turtle-fill.png';
+import turtleEmpty from '../img/turtle-empty.png';
+import starfish from '../img/starfish.png';
+import starfishFill from '../img/starfish-fill.png';
+import starfishEmpty from '../img/starfish-empty.png';
+import fish from '../img/fish.png';
+import fishFill from '../img/fish-fill.png';
+import fishEmpty from '../img/fish-empty.png';
+
+const Instructions = () => {
+  return <div id='instructions-container'></div>;
+};
+
+export default Instructions;
